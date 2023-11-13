0e0d96ac7f634e4f1e4ff4cbe7153a18afe13a2e
Gerrykoh Lampaug <134842445+gerrylamp@users.noreply.github.com>
2023-11-13 17:47:04 +0800

    Delete form.html directory

--- form.html/form only/script.js
+++
@@ -1,8 +0,0 @@
-var progress1 = document.getElementById('progress1');
-var progress2 = document.getElementById('progress2');
-var progress3 = document.getElementById('progress3');
-
-function next(){
-    progress1.classList.add('display-none');
-    progress2.classList.remove('display-none');
-}
